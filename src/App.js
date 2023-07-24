7e21f752c3446180ec3eab039dfbb811cc62d680
Tony <122675474+Noidan@users.noreply.github.com>
2023-07-24 13:13:05 +0300

    removeData

--- src/App.js
+++ src/App.js
@@ -17,9 +17,21 @@ function App() {
     setPosts([...posts, newPost])
   }
 
+  // get post from child component
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return <div className="App">
     <PostForm create={createPost} />
-    <PostList posts={posts} title='Список постов' />
+    {posts.length
+      ?
+      <PostList remove={removePost} posts={posts} title='Список постов' />
+      :
+      <h1 style={{ textAlign: 'center' }}>
+        Posts not found
+      </h1>
+    }
   </div>
 }
 
