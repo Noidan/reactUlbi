395f915ae5a5e391635bcd030eb45898ca788957
Tony <122675474+Noidan@users.noreply.github.com>
2023-07-24 12:54:48 +0300

    AddData

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { useState } from "react";
-import Counter from "./components/Counter";
+import './styles/App.css'
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 function App() {
 
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'Description 1' },
+    { id: 2, title: 'React', body: 'Description 2' },
+    { id: 3, title: 'Redux', body: 'Description 3' },
+    { id: 4, title: 'NodeJS', body: 'Description 4' },
+    { id: 5, title: 'TypeScript', body: 'Description 5' }
+  ])
 
-const [value, setValue]= useState ('TEXT')
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
 
-
-  return (
-
-    <div className="App">
-      <Counter/>
-      <Counter/>
-
-      <h1>{value}</h1>
-      <input 
-      type='text'
-      value={value}
-      onChange = {event => setValue(event.target.value)}
-      />
-
-    </div>
-  );
+  return <div className="App">
+    <PostForm create={createPost} />
+    <PostList posts={posts} title='Список постов' />
+  </div>
 }
 
 export default App;
