f7d6f006abb33cb4dd5bfaa9034ae51d8ebfc3a6
Tony <122675474+Noidan@users.noreply.github.com>
2023-07-25 11:26:09 +0300

    Memoization

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { useMemo, useState } from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
-import MyInput from "./components/UI/Input/MyInput";
+import PostFilter from "./components/PostFilter";
 
 function App() {
 
@@ -15,21 +14,20 @@ function App() {
     { id: 5, title: 'TypeScript', body: 'Description 5' }
   ])
 
-  const [selectedSort, setSelectedSort] = useState('')
-  const [searchQuery, setSearchQuery] = useState('')
+  const [filter, setFilter] = useState({ sort: '', query: '' })
 
   const sortedPosts = useMemo(() => {
-    if (selectedSort) {
+    if (filter.sort) {
       return [...posts].sort((a, b) =>
-        a[selectedSort].localeCompare(b[selectedSort]))
+        a[filter.sort].localeCompare(b[filter.sort]))
     }
     return posts
-  }, [selectedSort, posts])
+  }, [filter.sort, posts])
 
   const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
-
-  }, [searchQuery, sortedPosts])
+    return sortedPosts.filter(post =>
+      post.title.toLowerCase().includes(filter.query))
+  }, [filter.query, sortedPosts])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -40,37 +38,11 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  const sortPosts = (sort) => {
-    setSelectedSort(sort)
-  }
-
   return <div className="App">
     <PostForm create={createPost} />
     <hr style={{ margin: '15px 0' }} />
-    <div>
-      <MyInput
-        value={searchQuery}
-        onChange={e => setSearchQuery(e.target.value)}
-        placeholder='Search'
-      />
-      <MySelect
-        value={selectedSort}
-        onChange={sortPosts}
-        defaultValue="Sort by"
-        options={[
-          { value: 'title', name: 'name' },
-          { value: 'body', name: 'description' }
-        ]}
-      />
-    </div>
-    {sortedAndSearchedPosts.length
-      ?
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Post list' />
-      :
-      <h1 style={{ textAlign: 'center' }}>
-        Posts not found
-      </h1>
-    }
+    <PostFilter filter={filter} setFilter={setFilter} />
+    <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Post list' />
   </div>
 }
 
---
+++ src/components/UI/MyModal/MyModal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from './MyModal.module.css'
+
+const MyModal = () => {
+
+    return <div className="">
+
+    </div>
+}
+
+export default MyModal
