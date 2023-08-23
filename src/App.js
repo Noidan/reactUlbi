6d6bf8a4ce5dd3dc7833c763aa3648b9358987a1
Tony <122675474+Noidan@users.noreply.github.com>
2023-08-23 17:10:32 +0300

    fetching

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { usePosts } from "./hooks/usePosts";
 import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from './components/UI/Loader/Loader';
+import { useFetching } from "./hooks/useFetching";
 
 
 function App() {
@@ -17,7 +18,10 @@ function App() {
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-  const [isPostsLoading, setIsPostsLoading] = useState(false)
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts)
+  })
 
   useEffect(() => {
     fetchPosts()
@@ -26,16 +30,6 @@ function App() {
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal(false)
-  }
-
-  async function fetchPosts() {
-    setIsPostsLoading(true)
-    setTimeout(async() => {
-      const posts = await PostService.getAll();
-      setPosts(posts)
-      setIsPostsLoading(false)
-    }, 1000)
-
   }
 
   const removePost = (post) => {
@@ -52,8 +46,11 @@ function App() {
 
     <hr style={{ margin: '15px 0' }} />
     <PostFilter filter={filter} setFilter={setFilter} />
+    {postError &&
+    <h1>Error ${postError}</h1>
+    }
     {isPostsLoading
-      ? <div style = {{display: 'flex', justifyContent:'center', marginTop: 100}}> <Loader/> </div>
+      ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 100 }}> <Loader /> </div>
       : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Post list' />
     }
   </div>
